fbf8354e408bf92afe06d637d3d65f8ab810885b
necatimuslu <76621291+necatimuslu@users.noreply.github.com>
2022-02-25 17:29:24 +0300

    Update baseUrl.js

--- app/services/baseUrl.js
+++ app/services/baseUrl.js
@@ -1,5 +1,5 @@
 export const baseUrl = 'https://api.themoviedb.org/3';
 
-export const apiKey = 'e8b1fbc526699ed0d7184727771ac9a5';
+export const apiKey = 'apikeyAlmalısın';
 
 export const imageSrc = 'https://image.tmdb.org/t/p/w500/';
